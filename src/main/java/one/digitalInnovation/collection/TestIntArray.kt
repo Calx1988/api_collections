27f71831c7fa2b561b82c4e294dcb8da81eb52c3
Cássio Alexsander <cassioalexsander@gmail.com>
2021-04-22 17:10:11 -0300

    Declarando IntArray e iteração.

---
+++ src/main/java/one/digitalInnovation/collection/TestIntArray.kt
@@ -0,0 +1,25 @@
+package main.java.one.digitalInnovation.collection
+
+fun main() {
+    val values=IntArray(5);
+    values[0]=1;
+    values[1]=8;
+    values[2]=5;
+    values[3]=0;
+    values[4]=9;
+
+    println("Iterando por uma variável.")
+    for (value in values){
+        println(value)
+    }
+    
+    println("Iterando pelo forEach.")
+    values.forEach {
+        println(it)
+    }
+
+    println("Iterando pelo índice.")
+    for (index in values.indices){
+        println(values[index])
+    }
+}
